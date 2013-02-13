5089f67c7654e891568c9859b5bb4e2cedca5b1b
Aaron Leung <aaron.leung@moovweb.com>
2013-02-12 19:17:14 -0800

    Okay, this test thingie should be good to go now.

--- testdriver/main.go
+++ testdriver/main.go
@@ -16,43 +16,8 @@ import (
 )
 
 func main() {
-
-  // var (
-  //   generateScss    bool
-  //   checkTimestamps bool
-  //   spritesFolder      string
-  //   spriteSheetsFolder string
-  //   styleSheetsFolder  string
-  // )
-
-  // flag.BoolVar(&generateScss, "scss", true, "generate Sass/SCSS variables and mixins")
-  // flag.BoolVar(&checkTimestamps, "check-timestamps", true, "don't regenerate sprite-sheets if they're newer than their component sprite images")
-  // flag.StringVar(&spritesFolder, "sprites-folder", ".", "input folder containing subfolders with sprite images")
-  // flag.StringVar(&spriteSheetsFolder, "spritesheets-folder", ".", "output folder in which to deposit the sprite-sheets")
-  // flag.StringVar(&styleSheetsFolder, "stylesheets-folder", ".", "output folder in which to deposit the stylesheets")
-
-  // flag.Parse()
-
-
-  // var stylesheetExtension string
-  // if generateScss {
-  //   stylesheetExtension = ".scss"
-  // } else {
-  //   stylesheetExtension = ".css"
-  // }
-
-  // sheets, styles, _ := spracker.GenerateSpriteSheetsFromFolders(spritesFolder, spriteSheetsFolder, spriteSheetsFolder, generateScss, checkTimestamps, log)
-  // for i, sheet := range sheets {
-  //   wstErr := spracker.WriteSpriteSheet(sheet.Image, spriteSheetsFolder, sheet.Name, log)
-  //   if wstErr == nil {
-  //     log.Info("Generated sprite-sheet '%s.png'", filepath.Join(spriteSheetsFolder, sheet.Name))
-  //   }
-  //   wspErr := spracker.WriteStyleSheet(styles[i], styleSheetsFolder, sheet.Name+stylesheetExtension, log)
-  //   if wspErr == nil {
-  //     log.Info("Generated stylesheet '%s%s'", filepath.Join(styleSheetsFolder, sheet.Name), stylesheetExtension)
-  //   }
-  // }
-
+  os.RemoveAll("kittens")
+  os.Mkdir("kittens", 0755)
 
   log := golog.NewLogger("")
   log.AddProcessor("first", golog.NewConsoleProcessor(golog.LOG_INFO, true))
@@ -81,9 +46,9 @@ func main() {
 
     var fileName string
     if magFac != 1 {
-      fileName = fmt.Sprintf("k0%d@%#vx.jpg", i, magFac)
+      fileName = fmt.Sprintf("kittens/k0%d@%#vx.jpg", i, magFac)
     } else {
-      fileName = fmt.Sprintf("k0%d.jpg", i)
+      fileName = fmt.Sprintf("kittens/k0%d.jpg", i)
     }
 
     writeErr := ioutil.WriteFile(fileName, imgData, 0666)
@@ -92,8 +57,8 @@ func main() {
     }
   }
 
-  wd, _ := os.Getwd()
-  sheet, styles, _, spriteErr := spracker.GenerateSpriteSheetFromFolder(wd, "kittens", ".", false, false, log)
+  // wd, _ := os.Getwd()
+  sheet, styles, _, spriteErr := spracker.GenerateSpriteSheetFromFolder("kittens", "kittens", ".", false, false, log)
   if spriteErr != nil {
     log.Error("unable to generate spritesheet; please delete images and stylesheets and try again")
   } else {
